c5060ec9aef0469f08e86e98abbc6c9b61ab1982
WeiMing778 <23000077@myrp.edu.sg>
2024-07-31 11:52:02 +0800

    Database Credentials Updated

--- app.js
+++ app.js
@@ -223,5 +223,5 @@ app.get("/about", (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 3306
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
